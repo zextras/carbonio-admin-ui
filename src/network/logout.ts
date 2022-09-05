97bef7de697fed3bfc26de6fa5adc4e55cb00773
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-09-05 10:34:27 +0530

    fix: logout not remove auth cookies

--- src/network/logout.ts
+++ src/network/logout.ts
@@ -4,15 +4,9 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { SHELL_APP_ID } from '../constants';
-import { getSoapFetch } from './fetch';
 import { goToLogin } from './go-to-login';
 
 export const logout = (): Promise<void> =>
-	getSoapFetch(SHELL_APP_ID)('EndSession', {
-		_jsns: 'urn:zimbraAccount'
-	}).then(() => {
-		fetch('/?loginOp=logout')
-			.then((res) => res)
-			.then(goToLogin);
-	});
+	fetch('/logout')
+		.then((res) => res)
+		.then(goToLogin);
