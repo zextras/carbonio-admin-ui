cc0ed095b4717443ad356b9ec2466628244e9de2
zextras-vijay <vijay.godhasara@zextras.com>
2022-10-20 10:09:57 +0530

    fix: api cors issue

--- src/network/get-advance.ts
+++ src/network/get-advance.ts
@@ -5,11 +5,8 @@
  */
 import { useAdvanceStore } from '../store/advance';
 
-const hostname = window?.location?.hostname;
-const protocol = window?.location?.protocol;
-
 export const getIsAdvancedSupported = (): Promise<void> =>
-	fetch(`${protocol}//${hostname}/zx/auth/supported`)
+	fetch('/zx/auth/supported')
 		// eslint-disable-next-line consistent-return
 		.then((res) => {
 			if (res.status === 200) {
