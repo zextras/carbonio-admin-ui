ea1e36d4305a17d196267732e6a7d8bfb4520525
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-10-28 15:09:11 +0530

    chore: update nosession attribute

--- src/network/fetch.ts
+++ src/network/fetch.ts
@@ -139,10 +139,33 @@ export const getSoapFetch =
 		body: Request,
 		otherAccount?: string,
 		targetServer?: string,
-		authToken?: string
+		authToken?: string,
+		noSession = false
 	): Promise<Response> => {
 		const { zimbraVersion, account } = useAccountStore.getState();
 		const { context } = useNetworkStore.getState();
+		const header: any = {
+			context: {
+				_jsns: 'urn:zimbra',
+				notify: context?.notify?.[0]?.seq
+					? {
+							seq: context?.notify?.[0]?.seq
+					  }
+					: undefined,
+				session: context?.session ?? {},
+				account: getAccount(account as Account, otherAccount),
+				userAgent: {
+					name: userAgent,
+					version: zimbraVersion
+				},
+				targetServer: targetServer || undefined,
+				authToken: [{ _content: authToken }] || undefined
+			}
+		};
+		if (noSession) {
+			header.context.nosession = {};
+			delete header.context.session;
+		}
 		return fetch(`/service/admin/soap/${api}Request`, {
 			method: 'POST',
 			headers: {
@@ -152,24 +175,7 @@ export const getSoapFetch =
 				Body: {
 					[`${api}Request`]: body
 				},
-				Header: {
-					context: {
-						_jsns: 'urn:zimbra',
-						notify: context?.notify?.[0]?.seq
-							? {
-									seq: context?.notify?.[0]?.seq
-							  }
-							: undefined,
-						session: context?.session ?? {},
-						account: getAccount(account as Account, otherAccount),
-						userAgent: {
-							name: userAgent,
-							version: zimbraVersion
-						},
-						targetServer: targetServer || undefined,
-						authToken: [{ _content: authToken }] || undefined
-					}
-				}
+				Header: header
 			})
 		}) // TODO proper error handling
 			.then((res) => res?.json())
