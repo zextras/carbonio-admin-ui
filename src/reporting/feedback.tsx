32a62b46bdc1cd474cc50999fe0754c0fcb6a08a
mananpatel13589 <38516903+mananpatel13589@users.noreply.github.com>
2022-11-07 18:13:41 +0530

    chore: feedback ui changes

--- src/reporting/feedback.tsx
+++ src/reporting/feedback.tsx
@@ -20,7 +20,8 @@ import {
 	Container,
 	Row,
 	Icon,
-	SnackbarManagerContext
+	SnackbarManagerContext,
+	Padding
 } from '@zextras/carbonio-design-system';
 import { Severity, Event } from '@sentry/browser';
 import { filter, find, map } from 'lodash';
@@ -30,14 +31,16 @@ import { useUserAccount } from '../store/account';
 import { useRemoveCurrentBoard } from '../shell/boards/board-hooks';
 import { feedback } from './functions';
 import { useAppList } from '../store/app';
+import Logo from '../../assets/carbonio_feedback.svg';
 
 const TextArea = styled.textarea<{ size?: string }>`
 	width: 100%;
-	min-height: 200px;
+	height: 96%;
+	min-height: 96%;
 	box-sizing: border-box;
 	outline: none;
 	border: none;
-	background: ${({ theme }): string => theme.palette.gray5.regular};
+	background: ${({ theme }): string => theme.palette.gray4.regular};
 	resize: none;
 	transition: height 0.4s ease;
 	color: ${({ theme }): string => theme.palette.text.regular};
@@ -56,17 +59,19 @@ const TextContainer = styled(Container)`
 `;
 
 const ButtonContainer = styled(Container)`
-	width: 20%;
+	width: 100%;
 	position: relative;
+	padding-bottom: 32px;
 `;
 
 const TAContainer = styled(Container)`
-	background: ${({ theme }): string => theme.palette.gray5.regular};
+	background: ${({ theme }): string => theme.palette.gray4.regular};
 	border-radius: 2px 2px 0 0;
 	padding: 8px;
+	width: 100%;
 	transition: height 0.4s ease;
-	height: auto;
-	max-height: 80%;
+	height: 100%;
+	max-height: 100%;
 	&:focus-within {
 		background: ${({ theme }): string => theme.palette.gray4.regular};
 		outline: none;
@@ -130,6 +135,7 @@ const Feedback: FC = () => {
 	const [t] = useTranslation();
 	const topics = useMemo(() => getTopics(t), [t]);
 	const allApps = useAppList();
+	const [toggleFeedback, setToggleFeedback] = useState(false);
 	const apps = useMemo(
 		() => filter(allApps, (app) => !!app.sentryDsn),
 
@@ -169,7 +175,7 @@ const Feedback: FC = () => {
 		// eslint-disable-next-line no-param-reassign
 		ev.target.style.height = 'auto';
 		// eslint-disable-next-line no-param-reassign
-		ev.target.style.height = `${25 + ev.target.scrollHeight}px`;
+		// ev.target.style.height = `${25 + ev.target.scrollHeight}px`;
 		if (ev.target.value.length <= 500) {
 			setLimit(ev.target.value.length);
 			dispatch({ type: 'set-message', payload: ev.target.value });
@@ -195,16 +201,9 @@ const Feedback: FC = () => {
 
 	const confirmHandler = useCallback(() => {
 		const feedbackId = feedback(event);
-		createSnackbar(
-			feedbackId
-				? { type: 'success', label: t('feedback.success', 'Thank you for your feedback') }
-				: {
-						type: 'error',
-						label: t('feedback.error', 'There was an error while sending your feedback')
-				  }
-		);
+		setToggleFeedback(true);
 		// closeBoard();
-	}, [event, createSnackbar, t]);
+	}, [event]);
 
 	useEffect(() => {
 		dispatch({
@@ -216,28 +215,97 @@ const Feedback: FC = () => {
 	const disabledSend = useMemo(() => (event?.message?.length ?? 0) <= 0, [event?.message]);
 
 	return (
-		<Container padding={{ all: 'large' }} mainAlignment="space-around">
-			<TAContainer crossAlignment="flex-end">
-				<TextArea
-					value={event.message}
-					onKeyUp={checkTopicSelect}
-					onChange={onInputChange}
-					placeholder={t('feedback.write_here', 'Write your feedback here')}
-				/>
-				<Text size="medium" color="secondary">
-					{limit}/500
-				</Text>
-			</TAContainer>
-			<Container orientation="horizontal" height="fit">
-				<ButtonContainer crossAlignment="flex-end" mainAlignment="baseline">
-					<Button
-						label={t('feedback.send', 'SEND')}
-						onClick={confirmHandler}
-						disabled={disabledSend}
-					/>
-				</ButtonContainer>
-			</Container>
-		</Container>
+		<>
+			{!toggleFeedback ? (
+				<Container
+					padding={{ top: 'extralarge' }}
+					mainAlignment="space-between"
+					crossAlignment="flex-start"
+				>
+					<TAContainer crossAlignment="flex-end">
+						<TextArea
+							value={event.message}
+							onKeyUp={checkTopicSelect}
+							onChange={onInputChange}
+							placeholder={t(
+								'feedback.write_here_placeholder_text',
+								'Hey! Start writing here your feedback :) \n\n\nThis will be sent anonymously so let us know what we did good \nor wrong with no filter'
+							)}
+						/>
+						<Text size="medium" color="secondary">
+							{limit}/500
+						</Text>
+					</TAContainer>
+					<Container orientation="horizontal" height="fit" padding={{ top: 'large' }} width="100%">
+						<ButtonContainer crossAlignment="flex-end" mainAlignment="baseline">
+							<Button
+								width="fill"
+								label={t('feedback.send_feedback', 'SHARE YOUR FEEDBACK WITH US')}
+								onClick={confirmHandler}
+								disabled={disabledSend}
+							/>
+						</ButtonContainer>
+					</Container>
+				</Container>
+			) : (
+				<Container
+					padding={{ top: 'extralarge' }}
+					mainAlignment="space-between"
+					crossAlignment="flex-start"
+				>
+					<Container>
+						<Text overflow="break-word" weight="normal" size="large">
+							<Padding top="small" />
+							<Logo />
+						</Text>
+						<Padding vertical="large" horizontal="medium" width="294px" />
+						<Text
+							color="gray1"
+							overflow="break-word"
+							weight="normal"
+							size="large"
+							width="60%"
+							style={{ whiteSpace: 'pre-line', textAlign: 'center', paddingBottom: '123px' }}
+						>
+							<Text weight="bold">
+								{t('label.thank_you_for_feedback', `Thank you for the feedback!`)}
+							</Text>
+							<Padding bottom="large" />
+							<Text>
+								{t(
+									'label.post_feedback_send_helper_text_line_2',
+									`To improve Carbonio we’re running continuos testing sessions,`
+								)}
+							</Text>
+							<Text>
+								{t(
+									'label.post_feedback_send_helper_text_line_3',
+									`you can be the voice we’d love to hear right now!`
+								)}
+							</Text>
+							<Padding top="large" />
+							<Text>{t('label.feedback_helper_text', `Would you like to participate?`)}</Text>
+						</Text>
+					</Container>
+
+					<Container
+						orientation="horizontal"
+						height="fit"
+						padding={{ top: 'extralarge' }}
+						width="100%"
+					>
+						<ButtonContainer crossAlignment="flex-end" mainAlignment="baseline">
+							<Button
+								width="fill"
+								label={t('feedback.count_user_in_helper_button_text', 'YES, COUNT ME IN!')}
+								onClick={confirmHandler}
+								disabled={disabledSend}
+							/>
+						</ButtonContainer>
+					</Container>
+				</Container>
+			)}
+		</>
 	);
 };
 
