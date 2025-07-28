93d106906b6d51a02413d87323a57e081943d3dc
Davide Frison <davide.frison@zextras.com>
2025-07-28 11:42:35 +0200

    test: advanced supported pt.2

---
+++ src/network/isAdvancedSupported.ts
@@ -0,0 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+type Supported = {
+	supported: boolean;
+};
+type Error = {
+	errorMessage: string;
+};
+export const isAdvancedSupported = (): Promise<Supported | Error> =>
+	fetch('/advanced/supported').then(async (response: Response) => {
+		if (!response.ok) {
+			return {
+				errorMessage: 'Error'
+			};
+		}
+		const data = await response.json();
+		if ('supported' in data) return { supported: data.supported };
+		return { errorMessage: '' };
+	});
---
+++ src/network/tests/isAdvancedSupported.test.ts
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { HttpResponse } from 'msw';
+
+import { APIInterceptor, createAPIInterceptor } from '../../jest-env-setup';
+import { isAdvancedSupported } from '../isAdvancedSupported';
+
+const advancedSupportedApi = (supplier: () => HttpResponse): APIInterceptor =>
+	createAPIInterceptor('get', '/advanced/supported', supplier);
+
+describe('isAdvancedSupported', () => {
+	it('Should return true when the isAdvancedSupported is true', async () => {
+		advancedSupportedApi(() => HttpResponse.json({ supported: true }, { status: 200 }));
+
+		const advancedSupported = await isAdvancedSupported();
+
+		expect(advancedSupported).toEqual({ supported: true });
+	});
+
+	it('Should return false when the isAdvancedSupported is false', async () => {
+		advancedSupportedApi(() => HttpResponse.json({ supported: false }, { status: 200 }));
+
+		const advancedSupported = await isAdvancedSupported();
+
+		expect(advancedSupported).toEqual({ supported: false });
+	});
+});
