88379d53007b7f9eedefa910a04e853251063e19
Davide Frison <davide.frison@zextras.com>
2025-07-28 15:44:00 +0200

    refactor: call goToLogin in init

--- src/boot/init.ts
+++ src/boot/init.ts
@@ -9,6 +9,7 @@ import I18nFactory from '../i18n/i18n-factory';
 import { getAllConfig } from '../network/get-all-config';
 import { getInfo } from '../network/get-info';
 import { getMinMaxAPIVersion } from '../network/get-min-max-api-version';
+import { goToLogin } from '../network/go-to-login';
 import { isAdvancedSupported } from '../network/isAdvancedSupported';
 import { loginConfig } from '../network/login-config';
 import StoreFactory from '../redux/store-factory';
@@ -36,7 +37,10 @@ export const init = (
 			initialCalls = getInfo();
 		}
 		return initialCalls
-			.catch((error: Error) => ({ error: '' }))
+			.catch((error: Error) => {
+				goToLogin();
+				return { error: error.message };
+			})
 			.then(() => {
 				_i18nFactory.setLocale(
 					(
