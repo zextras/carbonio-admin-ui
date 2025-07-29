0800de7e692003ce43ca3712cb6f6430611da5bc
Keshav Bhatt <keshavnrj@gmail.com>
2025-07-29 18:29:30 +0530

    chore(jest): refine test file exclusions in Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,7 @@ export default {
 
 		// Exclude all test files and folders
 		'!src/**/*.test.{js,ts,jsx,tsx}',
+		'!src/*.test.{js,ts,jsx,tsx}',
 		'!src/**/*.spec.{js,ts,jsx,tsx}',
 		'!src/**/__tests__/**',
 		'!src/**/tests/**',
